d8d9b11a443a3be8efcc155f4808b201a5a9d917
Samantha Ambroise <ilkigin@gmail.com>
2019-01-30 18:32:34 -0500

    updated function

--- funtion-drills.js
+++ funtion-drills.js
@@ -1,5 +1,6 @@
 function createGreeting(){
-console.log("Hi, my name is Samantha and I'm 25 years old")
+  return "Hi, my name is Samantha and I'm 25 years old";
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
